7ab59ac5c7dfd9f958675ddd22d97cb9ad43d761
WalkerManalone <walkermanalone@gmail.com>
2024-06-12 13:18:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,23 @@
 import os
 import json
 import uuid
-from flask import Flask, jsonify, render_template_string
+from flask import Flask, render_template_string
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     new_uuid = str(uuid.uuid4())
-    vmess_link = f"vmess://{{\"v\":\"2\",\"ps\":\"my_v2ray\",\"add\":\"{os.getenv('RENDER_EXTERNAL_HOSTNAME')}\",\"port\":\"10086\",\"id\":\"{new_uuid}\",\"aid\":\"64\",\"net\":\"tcp\",\"type\":\"none\",\"host\":\"\",\"path\":\"\",\"tls\":\"\"}}"
+    vmess_link = f"vmess://{{\"v\":\"2\",\"ps\":\"my_v2ray\",\"add\":\"{os.getenv('RENDER_EXTERNAL_HOSTNAME')}\",\"port\":\"443\",\"id\":\"{new_uuid}\",\"aid\":\"64\",\"net\":\"ws\",\"type\":\"none\",\"host\":\"\",\"path\":\"/vmess\",\"tls\":\"\"}}"
     
     # 更新配置文件
-    with open('/app/config.json', 'r') as f:
+    config_path = '/etc/v2ray/config.json'
+    with open(config_path, 'r') as f:
         config = json.load(f)
     
     config['inbounds'][0]['settings']['clients'][0]['id'] = new_uuid
     
-    with open('/app/config.json', 'w') as f:
+    with open(config_path, 'w') as f:
         json.dump(config, f, indent=2)
     
     return render_template_string('''
@@ -27,7 +28,7 @@ def home():
         </head>
         <body>
             <h1>Your V2Ray VMess Link</h1>
-            <p>{{ vmess_link }}</p>
+            <a href="{{ vmess_link }}">{{ vmess_link }}</a>
         </body>
         </html>
     ''', vmess_link=vmess_link)
